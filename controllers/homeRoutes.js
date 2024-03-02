4cee6a53798e80a74712dc2b62d3c99879121e0d
stef <stefanyhobson@rocketmortgage.com>
2024-03-02 15:19:47 -0500

    homeroutes added

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,93 +1,72 @@
-const router = require('express').Router();
-const { post, User } = require('../models');
-const withAuth = require('../utils/auth');
+const express = require("express");
+const router = express.Router();
+const { User, Blog, Comment } = require('../../models');
 
+// Homepage route
 router.get('/', async (req, res) => {
   try {
-    // Get all posts and JOIN with user data
-    const postData = await post.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['first_name', 'last_name'],
-        },
-      ],
-    });
-
-    // Serialize data so the template can read it
-    const posts = postData.map((post) => post.get({ plain: true }));
-    let user;
-    if (req.session.logged_in) {
-      const userData = await User.findByPk(req.session.user_id, {
-        attributes: { exclude: ['password'] },
-      });
-
-      user = userData.get({ plain: true });
-    } else {
-      user = {};
-    }
-
-    // Pass serialized data and session flag into template
-    res.render('homepage', {
-      posts,
-      user,
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
+    const blogs = await Blog.findAll({ include: [User] });
+    const hbsBlogs = blogs.map(blog => blog.get({ plain: true }));
+    const loggedIn = req.session.user ? true : false;
+    res.render('homepage', { blogs: hbsBlogs, loggedIn, username: req.session.user?.username });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: "An error occurred", error });
   }
 });
 
-router.get('/post/:id', async (req, res) => {
-  try {
-    const postData = await post.findByPk(req.params.id, {
-      include: [
-        {
-          model: User,
-          attributes: ['first_name', 'last_name'],
-        },
-      ],
-    });
-
-    const post = postData.get({ plain: true });
-
-    res.render('post', {
-      ...post,
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// Use withAuth middleware to prevent access to route
-router.get('/profile', withAuth, async (req, res) => {
-  try {
-    // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: post }],
-    });
-
-    const user = userData.get({ plain: true });
-
-    res.render('profile', {
-      ...user,
-      logged_in: true,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
+// Login route
 router.get('/login', (req, res) => {
-  // If the user is already logged in, redirect the request to another route
-  if (req.session.logged_in) {
-    res.redirect('/homepage');
-    return;
+  if (req.session.user) {
+    return res.redirect('/dashboard');
   }
-
   res.render('login');
+});
+
+// Signup route
+router.get('/signup', (req, res) => {
+  res.render('signup');
+});
+
+// Dashboard route
+router.get('/dashboard', async (req, res) => {
+  try {
+    if (!req.session.user) {
+      return res.redirect('/login');
+    }
+    const userData = await User.findByPk(req.session.user.id, { include: [Blog, Comment] });
+    const hbsData = userData.get({ plain: true });
+    hbsData.loggedIn = req.session.user ? true : false;
+    res.render('dashboard', hbsData);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: "An error occurred", error });
+  }
+});
+
+// Blog details route
+router.get('/blogs/:id', async (req, res) => {
+  try {
+    if (!req.session.user) {
+      return res.redirect('/login');
+    }
+    const blogData = await Blog.findByPk(req.params.id, { include: [User, { model: Comment, include: [User] }] });
+    const dbBlog = blogData.get({ plain: true });
+    const loggedIn = req.session.user ? true : false;
+    if (dbBlog.userId != req.session.user.id) {
+      return res.render('comment', { hbsBlog: dbBlog, loggedIn, username: req.session.user.username });
+    } else {
+      res.render('updateDelete', { hbsBlog: dbBlog, loggedIn, username: req.session.user.username });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: "An error occurred", error });
+  }
+});
+
+// Default route
+router.get('*', (req, res) => {
+  res.redirect('/');
 });
 
 module.exports = router;
