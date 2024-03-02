60cf2fddb187ff885f54352312a619113c6c7f2b
stef <stefanyhobson@rocketmortgage.com>
2024-03-02 13:19:12 -0500

    FIXED BUG IN BLOGROUTES

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -1,83 +1,86 @@
-const router = express.Router();
 const express = require("express");
+const router = express.Router();
 const { User, Blog, Comment } = require('../../models');
 const withAuth = require('../../utils/auth.js');
 
-router.get("/", (req, res => {
-  Blog.findAll({include:[User, Comment]})
-  .then(dbBlogs => {
-    res.json(dbBlogs);
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json({ msg: "an error occurred", err});
-  });
+// Error handling function
+const handleErrors = (res, err) => {
+  console.log(err);
+  res.status(500).json({ msg: "An error occurred", err });
+};
 
-}));
+router.get("/", (req, res) => {
+  Blog.findAll({ include: [User, Comment] })
+    .then(dbBlogs => {
+      res.json(dbBlogs);
+    })
+    .catch(err => {
+      handleErrors(res, err);
+    });
+});
 
 router.get("/:id", (req, res) => {
-  Blog.findByPk(req.params.id,{include:[User, Comment]})
-    .then(dbBkig => {
+  Blog.findByPk(req.params.id, { include: [User, Comment] })
+    .then(dbBlog => {
       res.json(dbBlog);
     })
     .catch(err => {
-      console.log(err);
-      res.status(500).json({msg: "an error occurred", err});
+      handleErrors(res, err);
     });
 });
 
 router.post('/', (req, res) => {
-  if(!req.session.user) {
-    return res.status(401).json({msg:"Please login!"})
-  } 
-    Blog.create({
-      title:req.body.title,
-      content:req.body.content,
-      userId:req.session.user.id
-    })
+  if (!req.session.user) {
+    return res.status(401).json({ msg: "Please login!" });
+  }
+
+  Blog.create({
+    title: req.body.title,
+    content: req.body.content,
+    userId: req.session.user.id
+  })
     .then(newBlog => {
       res.json(newBlog);
     })
     .catch(err => {
-      console.log(err);
-      res.status(500).json({msg: "an error occurred", err});
+      handleErrors(res, err);
     });
-  });
+});
 
-  router.put("/:id", (req, res) => {
-    if(!req.session.user) {
-      return res.status(401).json({msg:"Please login!"})
+router.put("/:id", (req, res) => {
+  if (!req.session.user) {
+    return res.status(401).json({ msg: "Please login!" });
+  }
+
+  Blog.update(req.body, {
+    where: {
+      id: req.params.id
     }
-    Blog.update(req.body, {
-      where: {
-        id: req.params.id
-      }
-    })
+  })
     .then(updatedBlog => {
       res.json(updatedBlog);
     })
     .catch(err => {
-      console.log(err);
-      res.status(500).json({msg: "an error occurred", err});
+      handleErrors(res, err);
     });
-  });
+});
 
-  router.delete("/:id", (req, res) => {
-    if(!req.session.user) {
-      return res.status(401).json({msg:"Please login!"})
-    } 
-    Blog.destroy({
-      where: {
-        id: req.params.id
-      }
-    })  
+router.delete("/:id", (req, res) => {
+  if (!req.session.user) {
+    return res.status(401).json({ msg: "Please login!" });
+  }
+
+  Blog.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
     .then(deletedBlog => {
       res.json(deletedBlog);
-    })    
+    })
     .catch(err => {
-      console.log(err);
-      res.status(500).json({msg: "an error occurred", err});
-    });  
-  });
+      handleErrors(res, err);
+    });
+});
 
 module.exports = router;
