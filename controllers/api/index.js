9de76c248ddccc88c5d7270200b889186eb3659d
stef <stefanyhobson@rocketmortgage.com>
2024-03-02 14:05:48 -0500

    api/index..js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,20 @@
-const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const express = require('./express');
+const router = express.Router();
+const express = require('express');
 
+const userRoutes = require('./api/userRoutes.js');
 router.use('/users', userRoutes);
-router.use('/posts', express);
+
+const blogRoutes = require('./api/blogRoutes.js');
+router.use('/blogs', blogRoutes);
+
+const commentRoutes = require('./api/commentRoutes.js');
+router.use('/comments', commentRoutes);
+
+const homeRoutes = require('./homeRoutes.js');
+router.use('/', homeRoutes);
+
+router.get('/showsessions', (req, res) => {
+    res.json(req.session);
+});
 
 module.exports = router;
