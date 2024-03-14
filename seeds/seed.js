2d54c55ad77b31b2c936de78f29663f8b0ffdca0
stef <stefanyhobson@rocketmortgage.com>
2024-03-13 20:03:41 -0400

    updated seeds

--- seeds/seed.js
+++ seeds/seed.js
@@ -2,23 +2,23 @@ const sequelize = require("../config/connection");
 const { User, Blog, Comment } = require("../models");
 
 const users = [
-    { username: "haley", password: "haleypassword" },
-    { username: "toby", password: "tobypassword" },
-    { username: "brenda", password: "brendapassword" },
+    { username: "Tommy", password: "whateverworks1!" },
+    { username: "Shelia", password: "Eandtheartist2" },
+    { username: "Marvin", password: "Gayewhatsgoinon3*" },
 ];
 
 const blogs = [
-    { title: "My first post", content: "meow", userId: 1 },
-    { title: "My second post", content: "woof", userId: 1 },
-    { title: "Toby's first post", content: "hi i'm Tobias", userId: 2 },
-    { title: "Brenda's first post", content: "hi i'm brenda", userId: 3 },
+    { title: "Coding is hard to learn", content: "make sure you have time to study", userId: 1 },
+    { title: "If you learn how to code it will be rewarding", content: "everything is hard to learn starting out, but coding is harder", userId: 1 },
+    { title: "Be persistent", content: "You may need to spend more time learning", userId: 2 },
+    { title: "Dont give up", content: "keep trying and be persistent", userId: 3 },
 ];
 
 const comments = [
-    { body: "great post!", blogId: 1, userId: 1 },
-    { body: "i agree!", blogId: 3, userId: 2 },
-    { body: "well said!", blogId: 4, userId: 1 },
-    { body: "happy monday!", blogId: 2, userId: 3 },
+    { body: "It took me over 2 years to understand javascript!", blogId: 1, userId: 1 },
+    { body: "If you learn it will mean you dedicated all your available time to learning!", blogId: 3, userId: 2 },
+    { body: "If you quit you will never know if you are capable of learning to code! keep trying even when you dont understand.  Force yourself to keep going!", blogId: 4, userId: 1 },
+    { body: "At least you will learn as much as U can learn given your circumstances!", blogId: 2, userId: 3 },
 ];
 
 const plantSeeds = async () => {
